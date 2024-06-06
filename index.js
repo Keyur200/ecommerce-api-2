39d9374f96b60e8a3566a7c8f6cb2cd6d6e901e3
Keyur Bhimani <keyurbhimani2000@gmail.com>
2024-06-06 12:47:02 +0530

    done

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ import cookieParser from 'cookie-parser'
 const app = express();
 dotenv.config();
 
-app.use(cors({credentials:true, origin: 'http://localhost:3000'}))
+app.use(cors({ origin: ["https://ecommerce-plum-theta.vercel.app"], methods: ["POST","PUT","DELETE","GET"] ,credentials:true}))
 app.use(express.json())
 app.use(cookieParser())
+mongoose.connect(process.env.MONGO_URL).then(res=>console.log("MongoDB connected")).catch(err=>console.log(err))
 
 app.use('/', userRoute)
 app.use('/', categoryRoute)
 app.use('/', productRoute)
 
-mongoose.connect(process.env.MONGO_URL).then(res=>console.log("MongoDB connected")).catch(err=>console.log(err))
 app.listen(process.env.PORT,() => console.log(`Running on ${process.env.PORT} port.`))
